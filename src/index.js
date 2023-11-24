9e7b9ed75f783049877afd1ae1f1d7d097888399
Faizulhaque2021 <Faizulhaque2021@gmail.com>
2023-11-24 19:43:17 +0600

    Update commit

--- src/index.js
+++ src/index.js
@@ -4,58 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/*
-const express = require ("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(cors());
-app.use(express.json())
-
-app.get('/',(req,res)=>{
-  res.send('Portfolio is Running')
-})
-
-var DATABASENAME="portfoliodb";
-var Database;
-
-app.listen(3000,()=>{
-  MongoClient.connect(CONNECTION_STRING,(error,client)=>{
-   Database = client.db(DATABASENAME);
-  console.log(`Portfolio is Running, ${port}`)
-  })
-})
-
-
-const CONNECTION_STRING = "mongodb+srv://faizulhaque2021:Faiz@01713627446@portfolio.boq7zry.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(CONNECTION_STRING, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-*/
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
